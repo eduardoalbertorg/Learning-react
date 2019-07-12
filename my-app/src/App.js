7fda6e1f845fec816cf8dced185b5daa605ab98e
Eduardo Alberto Rodriguez Garcia <eduardoalbertorg@gmail.com>
2019-07-11 19:13:03 -0500

    Create HelloWorld class with method to change inner states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import './App.css';
 
-function HelloWorld(props) {
+/* function HelloWorld(props) {
   return (
     <div id="hello">
       <h3>{props.subtitle}</h3>
       {props.myText}
     </div>
   );
+} */
+
+class HelloWorld extends React.Component {
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.myText}
+          <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+      );
+    } else {
+      return (
+        <h1>
+          There are no elements
+          <button onClick={this.toggleShow}>
+            Toggle Show
+          </button>
+        </h1>
+      )
+    }
+  }
 }
 
 // Arrow function
