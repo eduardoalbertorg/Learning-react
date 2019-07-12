7681013d47597b7c8227b18f5e036033215dba9b
Eduardo Alberto Rodriguez Garcia <eduardoalbertorg@gmail.com>
2019-07-11 20:22:03 -0500

    Separate app functionality into components

--- tasks-app/src/App.js
+++ tasks-app/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import tasksList from './sample/tasks.json';
+
+import Tasks from './components/Tasks';
+
+class App extends Component {
+
+  state = {
+    tasksList: tasksList
+  }
+
+  render() {
+    return (
+      <div>
+        <Tasks tasksList={this.state.tasksList} />
+      </div>
+    )
+  }
 }
 
 export default App;
